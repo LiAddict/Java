aad7b27fc9b40b133d114a0ffcf99273434090df
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-06-07 13:51:46 -0700

    Create Percentages.java

---
+++ Percentages.java
@@ -0,0 +1,28 @@
+//Krista Elizabeth Ghazaleh
+//Chap3a
+//2/20/18
+import java.util.Scanner;
+
+public class Percentages {
+
+	public static void main(String[] args) 
+	{
+		double firstValue;
+		double secondValue;
+		Scanner inputDevice = new Scanner(System.in);
+		System.out.print("Enter a value>>");
+		firstValue = inputDevice.nextInt();
+		System.out.print("Enter another value>>");
+		secondValue = inputDevice.nextInt();
+		calculatePercent(firstValue, secondValue);
+		calculatePercent(secondValue, firstValue);
+	}
+
+	public static void calculatePercent(double num, double secondNum) 
+	{
+		double newNum;
+		newNum = ((num/secondNum)*(100));
+		System.out.println(num + " is " + newNum + "% of " + secondNum);
+	}
+
+}
