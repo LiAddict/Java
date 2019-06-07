a81ceecf487827d676880839c0f1db610ab8f8dc
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-06-07 13:58:11 -0700

    Create main DeclareSandwichIngredients.java

---
+++ main DeclareSandwichIngredients.java
@@ -0,0 +1,17 @@
+//Krista Elizabeth Ghazaleh
+//DeclareSandwichIngredients.java
+//2/20/18
+public class DeclareSandwichIngredients {
+
+	public static void main(String[] args)
+	{	Sandwich lunch = new Sandwich();
+		lunch.setIngredientType("tuna");
+		lunch.setBreadType("wheat");
+		lunch.setSandwichPrice(4.99);
+		System.out.println("You have ordered a " + lunch.getIngredientType() +
+				" sandwhich on " + lunch.getBreadType() + " bread, and the "
+						+ "price is $	" + lunch.getSandwichPrice());
+
+	}
+
+}
