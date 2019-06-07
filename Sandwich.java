006c44171767f6b0388c7dec59c4cd15907c26fd
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-06-07 13:57:38 -0700

    Create Sandwich.java

---
+++ Sandwich.java
@@ -0,0 +1,35 @@
+//Krista Elizabeth Ghazaleh
+//Sandwich.java
+//2/20/18
+public class Sandwich 
+{
+		private String breadType;
+		private String ingredientType;
+		private double sandwichPrice;
+		public double getSandwichPrice() 
+		{
+			return sandwichPrice;
+		}
+		public void setSandwichPrice(double sandwichPrice)
+		{
+			this.sandwichPrice = sandwichPrice;
+		}
+		public String getBreadType() 
+		{
+		return breadType;
+		}
+		public void setBreadType(String breadType)
+		{
+			this.breadType = breadType;
+		}
+		public String getIngredientType() 
+		{
+			return ingredientType;
+		}
+		public void setIngredientType(String ingredientType) 
+		{
+			this.ingredientType = ingredientType;
+		}
+		
+	}
+
