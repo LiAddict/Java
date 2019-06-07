efcdcf02aada7d4cd6c323412594e95e73907132
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-06-07 13:50:03 -0700

    Create CreatePurchase.java

---
+++ CreatePurchase.java
@@ -0,0 +1,52 @@
+//Written by Krista Elizabeth Ghazaleh
+//CreatePurchase
+//Written on 4/9/18
+import java.util.Scanner;
+public class CreatePurchase 
+{
+
+	public static void main(String[] args) 
+	{
+		int inVoice;
+		double salesTotal;
+		Purchase subject = new Purchase();
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter invoice number >>");
+		inVoice = input.nextInt();
+		if(inVoice < 1000||(inVoice>8000))
+		{	
+			System.out.println("Invoice number must be between 1000 and 8000");
+			System.out.print("Enter invoice number>>");
+			 		
+			inVoice = input.nextInt();
+			
+		} 
+	
+		
+		subject.setInvoiceNumber(inVoice);
+		do 
+		{
+		System.out.print("Enter sale amount>> ");
+		salesTotal = input.nextDouble();
+		}
+		while(salesTotal<0);
+		subject.setSaleAmount(salesTotal);
+			if(salesTotal>0)
+			{
+				
+			
+				
+			{
+			
+	while(salesTotal<0)
+		subject.setSaleAmount(salesTotal);
+	subject.displayInfo();
+			
+		
+			}
+		}
+			
+	}
+	
+}
+
